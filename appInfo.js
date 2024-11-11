c4fabae7b9f619b0c015bc96ee41afe897566cbf
Hugh Kemmer <70001406+xxm1730408007@users.noreply.github.com>
2024-11-11 14:51:08 +0800

    Add files via upload

--- appInfo.js
+++ appInfo.js
@@ -6,6 +6,6 @@ const pkg = require('./package.json');
 // 自定义 homePage，形如：https://youhost.com/aa/bb、 /aa/bb、../aa/bb
 // const subApp = helDevUtils.createVueSubApp(pkg, { npmCdnType: 'unpkg', homePage: './xx' });
 // 自定义 homePage，不拼接名字和版本号
-const subApp = helDevUtils.createVueSubApp(pkg, { npmCdnType: 'unpkg', homePage: 'http://47.108.251.34:12391/common'});
+const subApp = helDevUtils.createVueSubApp(pkg, { npmCdnType: 'unpkg', homePage: 'https://xxm1730408007.github.io/hel-comps-x/as_v1'});
 
 module.exports = subApp;
